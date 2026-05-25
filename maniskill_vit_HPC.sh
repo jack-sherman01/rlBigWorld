67f388bd705dcc8a213a6117a4152fb2b2aa9e37
Heng <myonly2015@163.com>
2026-05-25 17:14:39 +0200

    update

--- maniskill_vit_HPC.sh
+++ maniskill_vit_HPC.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=gpua
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12            # 3 processes × 4 OMP threads each
-#SBATCH --gpus=3
+#SBATCH --cpus-per-task=25            # 2 processes × 4 OMP threads each
+#SBATCH --gpus=4
 #SBATCH --time=24:00:00
-#SBATCH --mem=64G
+#SBATCH --mem=48G
 #SBATCH --output=logs/slurm_%j.out
 
 #SBATCH --mail-user=heng.zhang@iit.it
@@ -27,7 +27,7 @@ container_path=/work/hezhang/rlBigWorld/maniskill_vit.sif
 
 module load intel/singularity/singularity-4.2.2
 
-N_GPUS=3
+N_GPUS=2
 
 run_one() {
     local agent=$1 seed=$2 gpu=$3
